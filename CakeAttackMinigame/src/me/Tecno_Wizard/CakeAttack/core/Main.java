699e69dff1518f87ada7e53012808add58af509d
Ethan <ethanzeigler@gmail.com>
2015-02-02 21:44:54 -0500

    Created module for cakeattack, started basic setup

---
+++ CakeAttackMinigame/src/me/Tecno_Wizard/CakeAttack/core/Main.java
@@ -0,0 +1,21 @@
+package me.Tecno_Wizard.CakeAttack.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Ethan on 2/2/2015.
+ */
+public class Main extends JavaPlugin{
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    //register listeners
+
+}
