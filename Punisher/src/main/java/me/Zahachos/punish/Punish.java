775efa1edab124de79684a2fe092d6cadfc23083
Zahachos <zahachgaming@gmail.com>
2015-02-02 20:12:02 -0500

    complete config support added

--- Punisher/src/main/java/me/Zahachos/punish/Punish.java
+++ Punisher/src/main/java/me/Zahachos/punish/Punish.java
@@ -1,5 +1,6 @@
 package me.Zahachos.punish;
 
+import me.Zahachos.punish.managers.ConfigManager;
 import me.Zahachos.punish.managers.MessageManager;
 import org.bukkit.*;
 import org.bukkit.block.Block;
@@ -7,6 +8,7 @@ import org.bukkit.block.Skull;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -16,7 +18,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -26,51 +28,87 @@ public class Punish implements CommandExecutor {
     private ItemStack info, kick, mute, ban;
     public static OfflinePlayer playername;
 
-    public void openGUI(Player p) {
-        inv = Bukkit.getServer().createInventory(null, 9, "Choose a punishment type:");
+    FileConfiguration config = ConfigManager.getInstance().getConfig();
 
-        info = createItem(Material.SKULL_ITEM, "Player Info");
-        kick = createItem(Material.GLOWSTONE_DUST, "Kick/Warn");
-        mute = createItem(Material.SULPHUR, "Mute");
-        ban = createItem(Material.REDSTONE, "Ban");
+    public void openGUI(Player player, String inventoryName) {
 
-        inv.setItem(0, info);
-        inv.setItem(3, kick);
-        inv.setItem(5, mute);
-        inv.setItem(7, ban);
+        try {
+            inv = Bukkit.getServer().createInventory(null, (int) config.get("main.size"), (String) config.get("main.name"));
+        } catch (Exception exe) {
+            MessageManager.getInstance().severe(player, "Error! The plugin isn't configured correctly!");
+            return;
+        }
 
-        p.openInventory(inv);
+        int counterTest = 1;
+        while (config.isSet("main.items."+counterTest)) {
+            counterTest ++;
+        }
+
+        if (counterTest > 9) {
+            MessageManager.getInstance().severe(player, "Error! The config contains too many items to include!");
+            return;
+        }
+
+        int counter = 1;
+        String name;
+        Material m;
+        try {
+            while (config.get("main.items."+counter) != null) {
+                name = config.getString("main.items." +counter+".name");
+                name = name.replace("&", "§");
+                m = Material.getMaterial((String) config.get("main.items."+counter+".material"));
+                ItemStack item = createItem(inventoryName, m, name, counter);
+                int slot = config.getInt("main.items."+counter+".slot");
+                inv.setItem(slot, item);
+                counter++;
+            }
+        } catch(Exception exe) {
+            MessageManager.getInstance().severe(player, "Error! The items in the config aren't properly set!");
+            exe.printStackTrace();
+            return;
+        }
+
+        player.openInventory(inv);
         playername = null;
     }
 
-    private ItemStack createItem(Material m, String name) {
+    private ItemStack createItem(String inventoryName, Material m, String name, int number) {
         if (m.equals(Material.SKULL_ITEM)) {
-            ItemStack itemstack = getPlayerSkull(name);
+            ItemStack itemstack = getPlayerSkull(playername.getName());
             SkullMeta im = (SkullMeta) itemstack.getItemMeta();
-            im.setDisplayName(ChatColor.YELLOW + name);
+            im.setDisplayName(getName(inventoryName, number));
             im.setOwner(playername.getName());
-            im.setLore(Arrays.asList(ChatColor.GOLD + playername.getName(),
-                    ChatColor.YELLOW + "" + playername.getUniqueId(),
-                    ChatColor.RED + "Click the skull to see past infractions", ChatColor.RED
-                            + "from this player."));
+            im.setLore(getLore(inventoryName, number));
             itemstack.setItemMeta(im);
             return itemstack;
         }
         ItemStack i = new ItemStack(m, 1);
-        if (name.equals("Mute") || name.equals("Kick/Warn")
-                || name.equals("Ban")) {
-            ItemMeta im = i.getItemMeta();
-            im.setDisplayName(ChatColor.DARK_RED + name);
-            im.setLore(Arrays.asList(ChatColor.RED + "Click here to view "
-                    + name.toLowerCase(), ChatColor.RED
-                    + "punishment options."));
-            i.setItemMeta(im);
-            return i;
+        ItemMeta im = i.getItemMeta();
+        im.setDisplayName(getName(inventoryName, number));
+        im.setLore(getLore(inventoryName, number));
+        i.setItemMeta(im);
+        return i;
+    }
+    
+    public String getName(String inv, int itemID) {
+        String name = config.getString(inv+".items."+itemID+".name");
+        name = name.replace("&", "§").replace("%playername%", playername.getName()).replace("%uuid%", playername.getUniqueId().toString());
+        return name;
+    }
+    
+    public List<String> getLore(String inv, int itemID) {
+        List<String> lore = config.getStringList(inv+".items."+itemID+".lore");
+        int counterLore = 1;
+        while (counterLore <= lore.size()) {
+            String loreString = lore.get(counterLore-1).replace("&", "§").replace("%playername%", playername.getName()).replace("%uuid%", playername.getUniqueId().toString());
+            lore.remove(counterLore-1);
+            lore.add(counterLore-1, loreString);
+            counterLore++;
         }
-        return null;
+        return lore;
     }
 
-    public static ItemStack getPlayerSkull(String name) {
+    public ItemStack getPlayerSkull(String name) {
         Location l = Bukkit.getWorlds().get(0).getSpawnLocation();
         l.setY(0);
         Block b = l.getBlock();
@@ -85,6 +123,27 @@ public class Punish implements CommandExecutor {
         b.setType(m);
         b.setData(data);
         return is;
+    }
+    
+    public boolean hasPaid(String player, Player sender) {
+        try {
+            String URL = "http://www.minecraft.net/haspaid.jsp?user=" + player;
+            Document playerExists = Jsoup.connect(URL).get();
+            String inputLine = playerExists.select("body").toString();
+            Pattern pattern = Pattern.compile("\\s([A-Za-z]+)");
+            Matcher matcher = pattern.matcher(inputLine);
+            if (matcher.find()) {
+                if (matcher.group(1).equalsIgnoreCase("true")) {
+                    return true;
+                } else {
+                    MessageManager.getInstance().severe(sender, "Player doesn't exist!");
+                    return false;
+                }
+            }
+        } catch (IOException e) {
+            MessageManager.getInstance().severe(sender, "Connection timed out! Please try again!");
+        }
+        return false;
     }
 
     @SuppressWarnings("deprecation")
@@ -104,28 +163,13 @@ public class Punish implements CommandExecutor {
             }
 
             if (args.length > 1) {
-                MessageManager.getInstance().severe(player, "Too many arguments!");
-                return true;
+                MessageManager.getInstance().info(player, "No reason specified, using the config default.");
             }
-            try {
-                String URL = "http://www.minecraft.net/haspaid.jsp?user=" + args[0];
-                Document playerExists = Jsoup.connect(URL).get();
-                String inputLine = playerExists.select("body").toString();
-                Pattern pattern = Pattern.compile("\\s([A-Za-z]+)");
-                Matcher matcher = pattern.matcher(inputLine);
-                if (matcher.find()) {
-                    if (matcher.group(1).equalsIgnoreCase("true")) {
-                        playername = Bukkit.getServer().getOfflinePlayer(args[0]);
-                    } else {
-                        MessageManager.getInstance().severe(player, "Player doesn't exist!");
-                        return true;
-                    }
-                }
-            } catch (IOException e) {
-                MessageManager.getInstance().severe(player, "Connection timed out! Please try again!");
-                return true;
+            
+            if (hasPaid(args[0], player)) {
+                this.playername = Bukkit.getOfflinePlayer(args[0]);
+                openGUI(player, "main");
             }
-            openGUI(player);
         }
         return true;
     }
